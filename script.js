1fcbc3b1a7de51377be3156b692089676c6b64fe
Joshua Udeh <joshua.udeh@bemind.me>
2021-11-19 18:34:10 +0100

    mobile responsive

---
+++ script.js
@@ -0,0 +1,19 @@
+let paragraphs = Array.from(document.querySelectorAll('.first'));
+console.log(paragraphs);
+
+let projects = Array.from(document.querySelectorAll(".first-program")) ;
+console.log(projects);
+
+
+
+
+
+
+
+// function showInfo() {
+//     projects.classList.toggle('first-program');
+// }
+
+// paragraphs.addEventListener('click', showInfo);
+
+
